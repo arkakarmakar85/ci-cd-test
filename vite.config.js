693547cbd0af2c4e736f8581118b9f385c62b57d
arka <arka.karmakar@ibm.com>
2025-07-29 20:19:06 +0530

    ss

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Add this line to allow external access from your browser
+    host: '0.0.0.0', 
     port: '5173'
   },
 })
