1646781caed0121d590d6e0562bc3060bc823697
arka <arka.karmakar@ibm.com>
2025-07-29 22:58:56 +0530

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       
-      
+      <h1>This is a sample test for CI/CD in EC2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
